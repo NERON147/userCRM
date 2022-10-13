0a829fc90add347249af502faf4236dc58dd50af
NERON147 <faip98.07.5@gmail.com>
2022-10-13 17:44:35 +0400

    qq

---
+++ src/pages/404.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section>
+        <div class="container">
+            <h1>PAGE NOT FOUND!</h1>
+            <p>Go to<router-link class="link" to='/'> main page?</router-link></p> 
+        </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section>
+        <div class="container">
+            <h1>Home Page</h1>
+            {{message}}
+        </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    message () { //???
+      return this.$store.getters.getMessage
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
